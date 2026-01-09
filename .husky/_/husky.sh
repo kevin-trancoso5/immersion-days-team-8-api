09148763ec2dc294b817b9c1f06c715f9a6505eb
nicolas_costet <ncostet@deloitte.fr>
2026-01-09 15:04:21 +0100

    chore: update package.json for improved dependencies management

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+echo "husky - DEPRECATED
 
-# Minimal Husky helper sourced by hooks to ensure local binaries resolve.
-export PATH="$(pwd)/node_modules/.bin:$PATH"
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
