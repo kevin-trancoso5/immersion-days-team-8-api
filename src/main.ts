b15be578085ae1a19951520699125e53d0caed4c
Kevin Trancoso <kevin.trancoso5@gmail.com>
2026-01-12 11:30:08 +0100

    feat: add typeorm config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
   await app.listen(port);
 }
